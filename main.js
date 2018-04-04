3c9b1b3d14509d979dbe852055b35298721369f9
ValKiriann <hello@anaenriquez.es>
2018-04-04 13:40:21 +0200

    startirn the refactor and changing things...IS NOT WORKING NOW

--- main.js
+++ main.js
@@ -21,87 +21,107 @@
 // comprobar que el punto de inicio está dentro de la cuadricula
 // comprobar que tras las indicaciones el rover sigue dentro de la cuadricula
 
+// Comprueba que el número está dentro de la cuadrícula
+function isInGrid (number,gridSize, errorMessage){
+	if(number > gridSize) {
+		errorMessage.push("The coordinates of Rover can't be greater than grid size, please try again");
+	}
+	return errorMessage;
+}
+
+// Función para detectar que los datos numéricos son números y positivos
+function checkdataNumber(number, data, errorMessage) {
+	if(typeof gridSize != "number" && isNaN(Number(gridSize)) != false) {
+		errorMessage.push(data + " is not a number, please try again");
+	} else if(gridSize < 0){
+		errorMessage.push(data + " is not a positive number, please try again");
+	}
+	// QUIERO METER AQUI IS IN GRID PERO NO QUIERO AUMENTAR A 4 LOS PARAMETROS ASI QUE ME AGOBIO
+	return errorMessage;
+}
+
+
+
+// función para cambiar el punto de mira del rover
+function changeHeading(actualHead, direction) {
+	switch(actualHead) {
+		case "N":
+			if(direction === "L"){head = "E"}else if(direction === "R"){head = "W"}
+			break;
+		case "S":
+			if(direction === "L"){head = "W"}else if(direction === "R"){head = "E"}
+			break;
+		case "E":
+			if(direction === "L"){head = "S"}else if(direction === "R"){head = "N"}
+			break;
+		case "W":
+			if(direction === "L"){head = "N"}else if(direction === "R"){head = "S"}
+			break;	
+	}
+}
+
+// función para avanzar hacia delante
+function moveForward(actualHead) {
+	switch(actualHead) {
+		case "N":
+			y++;
+			break;
+		case "S":
+			y--;
+			break;
+		case "E":
+			x--;
+			break;
+		case "W":
+			x++;
+			break;	
+	}
+}
+
+
+
+// ESTA COMO LA MIERDA PIENSA OTRA VEZ
+// función para ejecutar las direcciones
+function applyPath(directions,roverPosition,head,x,y,errorMessage){
+	var count = 0;
+	while(count < directions.length){
+		if(directions[count] === "M"){
+			moveForward(head);
+		}else{
+			changeHeading(head,directions[count]);
+		}
+		count++;
+	}
+
+	isInGrid(x);
+	isInGrid(y);
+
+	if(x > gridSize || y > gridSize) {
+		errorMessage.push("the directions cause Rover to get out of the grid, please try again");
+	} else {
+		roverPosition = x + " " + y + " " + head;
+		return roverPosition;
+	}
+	
+}
 
 function setup(gridSize, startPoint, directions){
 
 	// declarando las variables, tamaño de cuadricula, posicion del rover, y cada coordenada por separado. Un contador para las instrucciones
-	var xTotal, yTotal = gridSize;
 	var roverPosition = startPoint.split(" ");
 	var x = roverPosition[0];
 	var y = roverPosition[1];
 	var head = roverPosition[2];
-	var count = 0;
-
-	// función para cambiar el punto de mira del rover
-	function changeHeading(actualHead, direction) {
-		switch(actualHead) {
-			case "N":
-				if(direction === "L"){head = "E"}else if(direction === "R"){head = "W"}
-				break;
-			case "S":
-				if(direction === "L"){head = "W"}else if(direction === "R"){head = "E"}
-				break;
-			case "E":
-				if(direction === "L"){head = "S"}else if(direction === "R"){head = "N"}
-				break;
-			case "W":
-				if(direction === "L"){head = "N"}else if(direction === "R"){head = "S"}
-				break;
-			
-		}
-	}
-
-	// función para avanzar hacia delante
-	function moveForward(actualHead) {
-		switch(actualHead) {
-			case "N":
-				y++;
-				break;
-			case "S":
-				y--;
-				break;
-			case "E":
-				x--;
-				break;
-			case "W":
-				x++;
-				break;
-			
-		}
-	}
-
-	// función para ejecutar las direcciones
-	function applyPath(){
-		count = 0;
-		while(count < directions.length){
-			if(directions[count] === "M"){
-				moveForward(head);
-			}else{
-				changeHeading(head,directions[count]);
-			}
-			count++;
-		}
-
-		if(x > gridSize || y > gridSize) {
-			console.error("the directions cause Rover to get out of the grid, please try again");
-		} else {
-			roverPosition = x + " " + y + " " + head;
-			return roverPosition;
-		}
-		
-	}
-
-	// PRIMERO TESTAMOS QUE LOS DATOS SON VÁLIDOS
 	var errorMessage = [];
 
-	// comprueba que el tamaño de la cuadrícula es un número y positivo
-	if(typeof gridSize != "number" && isNaN(Number(gridSize)) != false) {
-		errorMessage.push("gridSize is not a number, please try again");
-	} else if(gridSize < 0){
-		errorMessage.push("gridSize is not a positive number, please try again");
-	}
-
+	// Comprobar el tamaño de la cuadrícula
+	checkdataNumber(gridSize, "Grid Size");
 	//comprobar que startpoint es un dato válido
+	checkdataNumber(x, "X coordinate",errorMessage);
+	checkdataNumber(y, "Y coordinate",errorMessage);
+
+
+
 	if(typeof x != "number" && isNaN(Number(x)) != false) {
 		errorMessage.push("X coordinate for Rover is not a number, please try again");
 		console.log(isNaN(Number(x)));
